577bbc54480927a8aa93c5182683304d22be34e0
RogerChou <rogera95@gmail.com>
2013-12-23 09:52:50 +0800

    131223-1

--- Multitenant-apache-jmeter-2.9/clear.sh
+++
@@ -1,4 +0,0 @@
-cat /dev/null > TenantCnt.txt 
-echo 1 >> TenantCnt.txt 
-cat /dev/null > AssociationCnt.txt 
-echo 20000 >> AssociationCnt.txt 
--- Multitenant-apache-jmeter-2.9/run.sh
+++
@@ -1,3 +0,0 @@
-sh clear.sh 
-cp *.properties bin/ 
-java -jar bin/ApacheJmeter.jar -t TestPlan/Concurrent/CT80Update_test.jmx
